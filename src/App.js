5ee5bf9f81b905044ff0ec328d74c947dd61eb28
Tessa Hayden <tessahayden3@gmail.com>
2021-10-05 16:01:04 -0700

    reactstrap demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button
+} from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div className="App">
-        <Navbar light color="dark">
+        <Navbar light color="inverse" expand="md">
           <div className="container">
             <NavbarBrand href="/">Northwest Restaurant Consultant</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Portfolio</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="index.html">Services</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="contact.html">Contact</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col>
+                <h1>Northwest Restaurant Consultant</h1>
+                <p>
+                  <Button
+                    tag="a"
+                    color="success"
+                    size="large"
+                    href="index.html"
+                    target="_blank">Homepage</Button>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
       </div>
     );
   }
